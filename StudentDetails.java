e16c134e42a1821321ea9d37481de813a474aa26
Manovadhana M <113025165+Manojega0311@users.noreply.github.com>
2022-10-06 20:37:09 +0530

    Add files via upload

---
+++ StudentDetails.java
@@ -0,0 +1,20 @@
+//wrp to demo StudentDetails
+//Manovadhana M CG BATCH 2576
+import java.util.*;
+class StudentDetails
+{
+	int sid;
+	String sname;
+	double marks;
+}
+public class Student
+{
+	public static void main(String[]args)
+	{
+		StudentDetails s1 = new StudentDetails();
+		s1.sid = 6;
+		s1.sname = "Shravni";
+		s1.marks = 90;
+		System.out.println(s1.sid+" "+s1.sname+" "+s1.marks);
+	}
+}
